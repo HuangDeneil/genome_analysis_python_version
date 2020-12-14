a7226e890ed951e9e9686678b7c41865f2fbc872
hudeneil <h7106019005@smail.nchu.edu.tw>
2020-12-14 16:29:29 +0800

    day1

--- open_file/open_file.py
+++ open_file/open_file.py
@@ -1,27 +1,33 @@
 
 
 input_file_name = "codon_transfer.txt"
-i = ""
-readline = ""
-
-output_file = open("test.txt", mode = "w")  
-
-with open(input_file_name, mode = "r") as file:
-    for i in file:
-        readline = i.rstrip()   ## rstrip() 去換行
-        #print(readline)
-        output_file.write(readline+"\n")
-
-output_file.close()
+#output_file_name = "test.txt"
 
 # #https://ithelp.ithome.com.tw/articles/10161708
-# file = open(input_file_name, mode = "r" )
 
-# ## readlines in list
-# #print( file.readlines() )
+file = open(input_file_name, mode = "r" )
+# output_file = open(output_file_name , mode = "w")  
+
+#### Reading each lines into list
+print( file.readlines() )
 
 # for i in file.readlines():
 #     readline = i.rstrip()
 #     print(readline)
+    ## output_file.write(readline+"\n")
 
-# file.close()
+file.close()
+# output_file.close()
+
+
+
+i = ""
+readline = ""
+
+# with open(input_file_name, mode = "r") as file:
+#     for i in file:
+#         readline = i.rstrip()   ## rstrip() 去換行
+#         print(readline)
+
+
+
