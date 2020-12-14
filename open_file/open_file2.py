abd4c7d39a3e31f638f122cd1f0b762a0e1f1d9a
hudeneil <h7106019005@smail.nchu.edu.tw>
2020-12-14 18:05:26 +0800

    day1

--- open_file/open_file2.py
+++ open_file/open_file2.py
@@ -1,6 +1,5 @@
 import sys
-
-
+import re
 
 input_file_name = sys.argv[1]
 i = ""
@@ -9,6 +8,7 @@ count = 0
 
 readline_list = []
 amino_acid = {}
+codon_table = {}
 
 with open(input_file_name,mode = "r") as file:
     for i in file:
@@ -19,6 +19,13 @@ with open(input_file_name,mode = "r") as file:
         if len(readline_list) < 3:
             pass
         else:
+            
+            #print(readline_list[0])
+            try:
+                codon_table[readline_list[0]] = (readline_list[2])
+            except KeyError:
+                codon_table[readline_list[0]] = (readline_list[2])
+            
             try:
                 amino_acid[readline_list[1]] = int(amino_acid[readline_list[1]])+1
             except KeyError:
@@ -28,11 +35,44 @@ with open(input_file_name,mode = "r") as file:
             #print(amino_acid[readline_list[1]])
         #print(readline)
 
-for i in amino_acid.keys():
-    print(str(i) + "\t" + str(amino_acid[i]))
+# for i in amino_acid.keys():
+#     print(str(i) + "\t" + str(amino_acid[i]))
 
-print("Total: " + str(count) + " codon")
-print("Total: " + str(len(amino_acid.keys()) -1 ) + " amino acid & stop codon" )
+# print("Total: " + str(count) + " codon")
+# print("Total: " + str(len(amino_acid.keys()) -1 ) + " amino acid & stop codon" )
+
+
+input_file_name = sys.argv[2]
+with open(input_file_name,mode = "r") as file:
+    for i in file:
+        readline = i.rstrip()
+        
+        if re.findall( r"^>", readline):
+            print(readline)
+        else:
+            pass
+            seq = readline
+            #print(readline)
+            protein_seq = ""
+            for i in range(0, len(seq), 3): 
+                codon = seq[i:i + 3] 
+                try:
+                    protein_seq+=codon_table[codon]
+                except KeyError:
+                    protein_seq+=codon_table[codon]
+                sep=""
+                #print((codon_table[codon][0]))
+                #protein = sep.join(protein_seq)
+            print((protein_seq))
+        # #count = count+1
+        #readline_list = readline.split('\t')
+
+
+
+
+
+
+
 
 
 
